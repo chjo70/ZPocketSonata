faad92dafe9b866a3729b2564b645a061c4b4728
조철희 <chjo70@hotmail.com>
2020-08-27 09:44:26 +0900

    add

--- project-spec/meta-user/recipes-apps/apppocketsonata/Document/SQL 쿼리/makedb.sh
+++ project-spec/meta-user/recipes-apps/apppocketsonata/Document/SQL 쿼리/makedb.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
 echo "에미터 테이블을 생성합니다.===================================="
-echo "LOB 테이블을 생성합니다..."
-sqlite3 emitter.sqlite3 ".read LOBDATA.sql"
-sqlite3 emitter.sqlite3 ".read LOB_POSITION.sql"
-
-echo "ABT 테이블을 생성합니다..."
-sqlite3 emitter.sqlite3 ".read ABTDATA.sql"
-sqlite3 emitter.sqlite3 ".read ABT_POSITION.sql"
-
-echo "AET 테이블을 생성합니다..."
-sqlite3 emitter.sqlite3 ".read AETDATA.sql"
+sqlite3 emitter.sqlite3 ".read emitter.sql"
 
 echo "생성한 DB 를 복사합니다..."
 cp emitter.sqlite3 ../../files/SQLite3.DB/.
 cp emitter.sqlite3 ~/SQLite3.DB/.
 
+echo ""
 echo "CED/EOB 테이블을 생성합니다.===================================="
 echo "CED 관련 테이블을 생성합니다..."
 sqlite3 cedeob.sqlite3 ".read ced.sql"
@@ -23,5 +15,15 @@ sqlite3 cedeob.sqlite3 ".read ced.sql"
 echo "EOB 관련 테이블을 생성합니다..."
 sqlite3 cedeob.sqlite3 ".read eob.sql"
 
+echo "EOB 관련 코드를 생성합니다..."
+sqlite3 cedeob.sqlite3 ".read eob_code.sql"
+
+echo "CED/EOB VIEW 테이블을 생성합니다..."
+sqlite3 cedeob.sqlite3 ".read view.sql"
+
 cp cedeob.sqlite3 ../../files/SQLite3.DB/.
 cp cedeob.sqlite3 ~/SQLite3.DB/.
+
+echo ""
+echo "성공적으로 PocketSonata 관련 테이블을 생성했습니다."
+echo ""
