a317bbf80903fc81648cc45164a5aabea8d4e6e8
조철희 <chjo70@hotmail.com>
2022-01-11 20:56:00 +0900

    add

--- project-spec/meta-user/recipes-apps/apppocketsonata/MSC/RADARANL/build_run.bat
+++ project-spec/meta-user/recipes-apps/apppocketsonata/MSC/RADARANL/build_run.bat
@@ -1,11 +1,22 @@
 echo off
 
-copy ..\DEBUG\RADARANL.lib "\\192.168.0.41\Files\라이브러리\레이더 분석"
-copy ..\DEBUG\RADARANL.dll "\\192.168.0.41\Files\라이브러리\레이더 분석"
-copy ..\DEBUG\RADARANL.pdb "\\192.168.0.41\Files\라이브러리\레이더 분석"
-copy ..\RADARANLAPP\RadarAnl.ini "\\192.168.0.41\Files\라이브러리\레이더 분석"
-copy RadarAnlAlgorithm.h "\\192.168.0.41\Files\라이브러리\레이더 분석"
+REM X밴드 방탐기 ======================================
+IF NOT EXIST D:\RADAR\RADAR_DF\Debug goto ERROR
+copy ..\DEBUG\RADARANL.lib "D:\Radar\RADAR_RD\Debug"
+copy ..\DEBUG\RADARANL.dll "D:\Radar\RADAR_RD\Debug"
+copy ..\DEBUG\RADARANL.pdb "D:\Radar\RADAR_RD\Debug"
+rem copy ..\RADARANLAPP\RadarAnl.ini "\\192.168.0.41\Files\라이브러리\레이더 분석"
+rem copy RadarAnlAlgorithm.h "\\192.168.0.41\Files\라이브러리\레이더 분석"
 
 rem copy $(TargetDir)\$(TargetName).lib "z:\레이더 분석"
 rem copy $(TargetDir)\$(TargetName).dll "z:\레이더 분석"
 rem copy $(RemoteDebuggerWorkingDirectory)\RadarAnlAlgorithm.h "z:/레이더 분석"
+
+:ERROR
+Echo 이동할 폴더가 없어서 복사하지 못했습니다.
+goto END
+
+:DONE
+Echo 복사 완료 했습니다.
+
+:END
--- project-spec/meta-user/recipes-apps/apppocketsonata/MSC/RADARDIR/build_run.bat
+++ project-spec/meta-user/recipes-apps/apppocketsonata/MSC/RADARDIR/build_run.bat
@@ -1,11 +1,39 @@
 echo off
 
-rem
-copy ..\DEBUG\RADARDIR.lib "\\192.168.0.41\Files\라이브러리\레이더 방탐"
-copy ..\DEBUG\RADARDIR.dll "\\192.168.0.41\Files\라이브러리\레이더 방탐"
-copy ..\DEBUG\RADARDIR.pdb "\\192.168.0.41\Files\라이브러리\레이더 방탐"
-copy ..\RADARDIR\RadarDirAlgorithm.h "\\192.168.0.41\Files\라이브러리\레이더 방탐"
+
+REM X밴드 방탐기 ======================================
+IF NOT EXIST D:\RADAR\RADAR_DF\Debug goto ERROR
+copy ..\DEBUG\RADARDIR.lib "D:\RADAR\RADAR_DF\Debug"
+copy ..\DEBUG\RADARDIR.dll "D:\RADAR\RADAR_DF\Debug"
+copy ..\DEBUG\RADARDIR.pdb "D:\RADAR\RADAR_DF\Debug"
+copy ..\DEBUG\RADARDIR.lib "D:\RADAR\RADAR_RD\Debug"
+copy ..\DEBUG\RADARDIR.dll "D:\RADAR\RADAR_RD\Debug"
+copy ..\DEBUG\RADARDIR.pdb "D:\RADAR\RADAR_RD\Debug"
+copy ..\RadarDirAlgorithm.h "\\192.168.0.41\Files\라이브러리\레이더 방탐"
+goto DONE
+
+REM X밴드 방탐기 ======================================
+IF NOT EXIST \\192.168.0.41\Files\라이브러리\레이더 방탐 goto ERROR
+copy ..\DEBUG\RADARDIR.lib "D:\RADAR\RADAR_DF\Debug"
+copy ..\DEBUG\RADARDIR.dll "D:\RADAR\RADAR_DF\Debug"
+copy ..\DEBUG\RADARDIR.pdb "D:\RADAR\RADAR_DF\Debug"
+copy ..\DEBUG\RADARDIR.lib "D:\RADAR\RADAR_RD\Debug"
+copy ..\DEBUG\RADARDIR.dll "D:\RADAR\RADAR_RD\Debug"
+copy ..\DEBUG\RADARDIR.pdb "D:\RADAR\RADAR_RD\Debug"
+copy ..\RadarDirAlgorithm.h "\\192.168.0.41\Files\라이브러리\레이더 방탐"
+goto DONE
+rem copy ..\DEBUG\RADARDIR.pdb "\\192.168.0.41\Files\라이브러리\레이더 방탐"
+rem copy ..\RADARDIR\RadarDirAlgorithm.h "\\192.168.0.41\Files\라이브러리\레이더 방탐"
 
 rem copy $(TargetDir)\$(TargetName).lib "z:\레이더 방탐"
 rem copy $(TargetDir)\$(TargetName).dll "z:\레이더 방탐"
 rem copy $(RemoteDebuggerWorkingDirectory)\RadarDirAlgorithm.h "z:\레이더 방탐"
+
+:ERROR
+Echo 이동할 폴더가 없어서 복사하지 못했습니다.
+goto END
+
+:DONE
+Echo 복사 완료 했습니다.
+
+:END
