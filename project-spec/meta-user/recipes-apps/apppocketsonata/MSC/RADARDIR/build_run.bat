49c9077328100fbe4a326b9ac1030c1fd30f444a
조철희 <chjo70@hotmail.com>
2022-01-14 01:12:23 +0900

    add

--- project-spec/meta-user/recipes-apps/apppocketsonata/MSC/RADARDIR/build_run.bat
+++ project-spec/meta-user/recipes-apps/apppocketsonata/MSC/RADARDIR/build_run.bat
@@ -1,24 +1,33 @@
 echo off
 
 
+SET RADAR_DF_LIB=E:\GitHub\RADAR_DF\Debug
+SET RADAR_DF_HEADER=E:\GitHub\RADAR_DF\ICAA
+
+SET RADAR_RD_LIB=E:\GitHub\RADAR_RD\Debug
+SET RADAR_RD_HEADER=E:\GitHub\RADAR_RD\ICAA
+
 REM X밴드 방탐기 ======================================
-IF NOT EXIST D:\RADAR\RADAR_DF\Debug goto ERROR
-echo 레이더 방탐 라이브러리
-copy ..\DEBUG\RADARDIR.lib "D:\RADAR\RADAR_DF\Debug"
-copy ..\DEBUG\RADARDIR.dll "D:\RADAR\RADAR_DF\Debug"
-copy ..\DEBUG\RADARDIR.pdb "D:\RADAR\RADAR_DF\Debug"
+IF NOT EXIST %RADAR_DF_LIB% goto ERROR
+echo ===== 레이더 방탐 라이브러리 (%RADAR_DF_LIB%)
+copy ..\DEBUG\RADARDIR.lib %RADAR_DF_LIB%
+copy ..\DEBUG\RADARDIR.dll %RADAR_DF_LIB%
+copy ..\DEBUG\RADARDIR.pdb %RADAR_DF_LIB%
 
-echo 레이더 방탐 헤더 파일
-copy RadarDirAlgorithm.h "D:\RADAR\RADAR_DF\ICAA"
+echo.
+echo ===== 레이더 방탐 헤더 파일 (%RADAR_DF_HEADER%)
+copy RadarDirAlgorithm.h "%RADAR_DF_HEADER%"
 
-IF NOT EXIST D:\RADAR\RADAR_RD\Debug goto ERROR
-echo 레이더 방탐 라이브러리
-copy ..\DEBUG\RADARDIR.lib "D:\RADAR\RADAR_RD\Debug"
-copy ..\DEBUG\RADARDIR.dll "D:\RADAR\RADAR_RD\Debug"
-copy ..\DEBUG\RADARDIR.pdb "D:\RADAR\RADAR_RD\Debug"
+IF NOT EXIST %RADAR_RD_LIB% goto ERROR
+echo.
+echo ===== 레이더 방탐 라이브러리 (%RADAR_RD_LIB%)
+copy ..\DEBUG\RADARDIR.lib %RADAR_RD_LIB%
+copy ..\DEBUG\RADARDIR.dll %RADAR_RD_LIB%
+copy ..\DEBUG\RADARDIR.pdb %RADAR_RD_LIB%
 
-echo 레이더 방탐 헤더 파일
-copy RadarDirAlgorithm.h "D:\RADAR\RADAR_RD\ICAA"
+echo.
+echo ===== 레이더 방탐 헤더 파일 (%RADAR_RD_HEADER%)
+copy RadarDirAlgorithm.h %RADAR_RD_HEADER%
 
 goto DONE
 
@@ -40,10 +49,12 @@ rem copy $(TargetDir)\$(TargetName).dll "z:\레이더 방탐"
 rem copy $(RemoteDebuggerWorkingDirectory)\RadarDirAlgorithm.h "z:\레이더 방탐"
 
 :ERROR
+echo.
 Echo 이동할 폴더가 없어서 복사하지 못했습니다.
 goto END
 
 :DONE
-Echo 복사 완료 했습니다.
+echo.
+Echo ***** 복사 완료 했습니다.
 
 :END
