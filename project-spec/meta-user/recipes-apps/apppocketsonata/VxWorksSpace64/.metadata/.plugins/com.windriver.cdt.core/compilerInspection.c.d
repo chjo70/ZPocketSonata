c3a060caaf32555310c1cfb32eb9e3331e3f3c4a
조철희 <chjo70@hotmail.com>
2022-02-14 20:26:58 +0900

    Add

---
+++ project-spec/meta-user/recipes-apps/apppocketsonata/VxWorksSpace64/.metadata/.plugins/com.windriver.cdt.core/compilerInspection.c.d
@@ -0,0 +1,2 @@
+compilerInspection.o: \
+ D:/VirtualBoxVMs/Git/ZPocketSonata1/project-spec/meta-user/recipes-apps/apppocketsonata/VxWorksSpace64/.metadata/.plugins/com.windriver.cdt.core/compilerInspection.c
