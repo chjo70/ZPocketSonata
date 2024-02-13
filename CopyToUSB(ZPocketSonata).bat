13b34521065bca1fc78f83a4d383f22a5cbf8c5a
조철희 <chjo70@hotmail.com>
2024-02-13 23:52:33 +0900

    add

--- CopyToUSB(ZPocketSonata).bat
+++ CopyToUSB(ZPocketSonata).bat
@@ -2,25 +2,27 @@ echo off
 
 cls
 
-color 17
-
 REM ########################################################################
+color 17
 ECHO.
 ECHO Files 폴더를 복사 합니다.
-xcopy .\dev\files \RADAR\ZPocketSonata\project-spec\meta-user\recipes-apps\apppocketsonata\files /E/C/I/Y/D/F/U
+xcopy .\dev\files ..\RADAR\ZPocketSonata\project-spec\meta-user\recipes-apps\apppocketsonata\files /E/C/I/Y/D/F/U
 
 REM ########################################################################
+color 27
 ECHO.
 ECHO Documents 폴더를 복사 합니다.
-xcopy .\dev\Document \RADAR\ZPocketSonata\project-spec\meta-user\recipes-apps\apppocketsonata\Document /E/C/I/Y/D/F/U
+xcopy .\dev\Document ..\RADAR\ZPocketSonata\project-spec\meta-user\recipes-apps\apppocketsonata\Document /E/C/I/Y/D/F/U
 
 REM ########################################################################
+color 47
 ECHO.
 ECHO MSC 폴더를 복사 합니다.
-xcopy .\dev\MSC \RADAR\ZPocketSonata\project-spec\meta-user\recipes-apps\apppocketsonata\MSC /E/C/I/Y/D/F/U
+xcopy .\dev\MSC ..\RADAR\ZPocketSonata\project-spec\meta-user\recipes-apps\apppocketsonata\MSC /E/C/I/Y/D/F/U
 
 color
 
 ECHO.
 ECHO.
-pause
+rem pause
+timeout 10
